c393ed261cbc787318df42d79864426d8dbacc0f
ttr3dp <git@ttr3dp.net>
2022-09-25 13:15:31 +0200

    Patch: Xresources

--- config.h
+++ config.h
@@ -7,17 +7,25 @@ static int centered  = 0;                      /* -c option; centers dmenu on sc
 static int min_width = 500;                    /* minimum width when centered */
 static int border_width = 1;
 /* -fn option overrides fonts[0]; default X11 font or font set */
+
+
+static char font[] = "monospace:pixelsize=14";
 static const char *fonts[] = {
-	"monospace:pixelsize=14",
+	font,
 	"Noto Color Emoji:pixelsize=11:antialias=true:autohint=true",
 	"JoyPixels:pixelsize=11:antialias=true:autohint=true"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
+static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static char normfgcolor[] = "#bbbbbb";
+static char normbgcolor[] = "#222222";
+static char selfgcolor[]  = "#eeeeee";
+static char selbgcolor[]  = "#005577";
+static char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { normfgcolor, normbgcolor },
+	[SchemeSel]  = { selfgcolor,  selbgcolor  },
+	[SchemeOut]  = { "#000000",   "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines          = 0;
@@ -30,4 +38,21 @@ static unsigned int min_lineheight = 8;
  */
 static const char worddelimiters[] = " ";
 
-/* Size of the window border */
+/*
++ * Xresources preferences to load at startup
++ */
+ResourcePref resources[] = {
+	{ "font",         STRING, &font },
+	{ "normfgcolor",  STRING, &normfgcolor },
+	{ "normbgcolor",  STRING, &normbgcolor },
+	{ "selfgcolor",   STRING, &selfgcolor },
+	{ "selbgcolor",   STRING, &selbgcolor },
+	{ "prompt",       STRING, &prompt },
+	{ "topbar",       STRING, &topbar },
+	{ "fuzzy",        INTEGER, &fuzzy },
+	{ "centered",     INTEGER, &centered },
+	{ "min_width",    INTEGER, &min_width },
+	{ "border_width", INTEGER, &border_width },
+	{ "lineheight", INTEGER, &lineheight },
+	{ "min_lineheight", INTEGER, &min_lineheight },
+};
